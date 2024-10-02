383cea467a56b6765d1524efdce658e7a74d4653
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-10-02 23:57:10 +0000

    new div's

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1>What needs to be done today</h1>
-            <h1>What to get done before days end</h1>
-            <h1>Small todo's that can wait til tomarrow</h1>
-            <h1>What can be done tomarrow</h1>
+			
 		</div>
 	);
 };
