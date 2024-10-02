8b13b4be9ce9a00f464dc3e0d997218ee1cd86a4
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-10-02 22:18:21 +0000

    first save

---
+++ src/js/component/ToDoList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="text-center">
+			<h1>What needs to be done today</h1>
+            <h1>What to get done before days end</h1>
+            <h1>Small todo's that can wait til tomarrow</h1>
+            <h1>What can be done tomarrow</h1>
+		</div>
+	);
+};
+
+export default Home;
